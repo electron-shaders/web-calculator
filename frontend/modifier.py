6e985df95abe3267c841a07b325718d75df921ed
electron-shaders <1367922451@qq.com>
2021-10-12 10:26:34 +0800

    取消动态baseURL

--- frontend/modifier.py
+++ frontend/modifier.py
@@ -9,3 +9,11 @@ with open("index.html", "r", encoding="utf-8") as f1,open("%s.bak" % "index.html
         f2.write(line)
 os.remove("index.html")
 os.rename("%s.bak" % "index.html", "index.html")
+
+with open("src/utils/axios.js", "r", encoding="utf-8") as f1,open("%s.bak" % "src/utils/axios.js", "w", encoding="utf-8") as f2:
+    for line in f1:
+        if 'http://localhost:3001' in line:
+            line = line.replace('http://localhost:3001', 'https://demo.xn--ftwm9mhr4a.com')
+        f2.write(line)
+os.remove("src/utils/axios.js")
+os.rename("%s.bak" % "src/utils/axios.js", "src/utils/axios.js")
